f9693f96958c253aa634febc209a8cee4f01c29e
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-22 21:44:32 +0530

    📦 NEW: Build a Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Container, Flex, VStack } from "@chakra-ui/react";
 
-const Home: NextPage = () => {
-  return <h1>Hello Next.js 👋</h1>;
+const IndexPage = () => {
+  return (
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        ></VStack>
+      </Flex>
+    </Container>
+  );
 };
 
-export default Home;
+export default IndexPage;
