be2cc7d6294b2bb471ed5b98e3de3baa51953d1c
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-23 07:58:43 +0530

    📦 NEW: Implement Responsive Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import { Container, Flex } from "@chakra-ui/react";
-import Cart from "../src/sections/cart";
-import Details from "../src/sections/details";
+import { Container, Flex } from '@chakra-ui/react';
+import Cart from '../src/sections/cart';
+import Details from '../src/sections/details';
 
 const IndexPage = () => {
   return (
     <Container maxW="container.xl" p={0}>
-      <Flex h="100vh" py={20}>
+      <Flex
+        h={{ base: 'auto', md: '100vh' }}
+        py={[0, 10, 20]}
+        direction={{ base: 'column-reverse', md: 'row' }}
+      >
         <Details />
         <Cart />
       </Flex>
